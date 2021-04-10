bbcaca17bb338c20872419200caf7e9600124a9a
Agustin <broggiagustin13@outlook.com.ar>
2021-04-10 11:03:32 -0300

    Ejercicio 2 de php

---
+++ prog1.php
@@ -0,0 +1,20 @@
+<?php
+<<<<<<< HEAD
+	$primero = 16;
+    $segundo = 8;
+
+    $suma = $primero + $segundo;
+
+    echo "El resultado es:",$suma;	
+=======
+
+	$numero = 0;
+
+	for ($i=0; $i < 10; $i++) { 
+		echo $numero;
+		$numero = $numero + 1;
+	}
+
+	echo $numero;
+>>>>>>> 4330aa0 (Ejercicio 2 de php)
+?>
