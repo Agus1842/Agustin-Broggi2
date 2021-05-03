f99f3506bb1769a0d37cfec57b401a5811271f1e
Agustin <broggiagustin13@outlook.com.ar>
2021-05-03 14:51:41 -0300

    Ejercicio 5 a medias

---
+++ ejercicio5/php/prog.php
@@ -0,0 +1,10 @@
+<?php
+
+$Nombre = $_GET['nombre'];
+$Apellido = $_GET['apellido'];
+$DNI = $_GET['dni'];
+
+
+echo "El nombre es:", $Nombre,"</br>";
+echo "El Apellido es:", $Apellido,"</br>";
+echo "El dni es:", $DNI,"</br>";
